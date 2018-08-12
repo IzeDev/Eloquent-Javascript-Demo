d3abd2fb524ee9e4a2599d047f56e42d064b26af
Jimmy <jimmy.gustafsson@live.com>
2018-08-12 17:43:18 +0200

    Made changes

--- Chapter 18 Forms and Form fields/Autocompletion/Main.js
+++ Chapter 18 Forms and Form fields/Autocompletion/Main.js
@@ -1,14 +1,34 @@
 let names = ["Adam", "Jimmy", "Mirre", "Isa"];
+let textbox;
+let dropdown;
 
-function main() {
-    let textbox = document.querySelector("#textboxinput");
-    let dropdown = document.querySelector("#textboxdropdown");
 
-    for (let index = 0; index < names.length; index++) {
+function getNameMatches(input) {
+    let matches = names.filter(name => name.toLowerCase().startsWith(input.toLowerCase()));
+    return matches;
+}
+
+function showMatches(matches)
+{
+    for (let index = 0; index < matches.length; index++) {
         let option = document.createElement("option");
-        option.text = names[index];
+        option.text = matches[index];
         dropdown.add(option, null);
     }
+}
+
+function newInput(event) {
+    debugger;
+    let matches = getNameMatches(textbox.value);
+    showMatches(matches);        
+}
+
+function main() {
+    // textbox = new HTMLTextAreaElement();
+    textbox = document.querySelector("#textboxinput");
+    dropdown = document.querySelector("#textboxdropdown");
+
+    textbox.addEventListener("keyup", newInput);
 }
 
 
