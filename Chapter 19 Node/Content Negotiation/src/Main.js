a62f5a358c341b70d064d59e0258f6a849ccd8c1
Jimmy <jimmy.gustafsson@live.com>
2018-09-01 10:18:45 +0200

    Finished assignment

--- Chapter 19 Node/Content Negotiation/src/Main.js
+++ Chapter 19 Node/Content Negotiation/src/Main.js
@@ -1,13 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var http = require("http");
-var requst = http.request({
-    hostname: "https://eloquentjavascript.net/authors",
-    port: 443,
-    method: "GET"
-}, function (response) {
-    response.on("data", function (chunk) {
-        process.stdout.write(chunk.toString());
+function sendHttpRequest(hostname, path, contentType) {
+    var requst = http.request({
+        hostname: hostname,
+        path: path,
+        port: 80,
+        method: "GET",
+        headers: { Accept: contentType }
+    }, function (response) {
+        response.on("data", function (chunk) {
+            console.log(chunk.toString());
+        });
     });
-});
-requst.end();
+    requst.end();
+}
+sendHttpRequest("eloquentjavascript.net", "/authors", "text/html");
+sendHttpRequest("eloquentjavascript.net", "/authors", "text/plain");
+sendHttpRequest("eloquentjavascript.net", "/authors", "application/json");
+sendHttpRequest("eloquentjavascript.net", "/authors", "text/xml");
--- Chapter 19 Node/Content Negotiation/src/Main.ts
+++ Chapter 19 Node/Content Negotiation/src/Main.ts
@@ -1,11 +1,20 @@
-import * as http from "http"
-let requst = http.request({
-    hostname : "https://eloquentjavascript.net/authors",
-    port : 443,
-    method : "GET"
-}, function (response) {
-    response.on("data", function(chunk) {
-        process.stdout.write(chunk.toString());
+import * as http from "http";
+function sendHttpRequest(hostname, path, contentType) {
+    let requst = http.request({
+        hostname : hostname,
+        path : path,
+        port : 80,
+        method : "GET",
+        headers : {Accept : contentType}
+    }, function (response) {
+        response.on("data", function(chunk) {
+            console.log(chunk.toString());
+        });
     });
-});
-requst.end();
+    requst.end();
+}
+
+sendHttpRequest("eloquentjavascript.net", "/authors", "text/html");
+sendHttpRequest("eloquentjavascript.net", "/authors", "text/plain");
+sendHttpRequest("eloquentjavascript.net", "/authors", "application/json");
+sendHttpRequest("eloquentjavascript.net", "/authors", "text/xml");
